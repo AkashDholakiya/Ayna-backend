b079f6080c4b28a70c140cd1c91e0ddb099a5149
AkashDholakiya <akashdholakiya222@gmail.com>
2025-02-18 10:16:29 +0530

    strapi-backend-completion

--- src/index.js
+++ src/index.js
@@ -16,5 +16,37 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const { Server } = require('socket.io');
+    const io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['my-custom-header'],
+        credentials: true,
+      },
+    });
+
+    io.on('connection', (socket) => {
+
+      socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+      });
+
+      socket.on('sendMsg', ({ roomId, message }) => {
+        const ans = {
+          id: new Date().getTime(),
+          uid: socket.id,
+          msg: message.msg,
+          role: 'server',
+        };
+
+        io.to(roomId).emit('recvMsg', ans);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+      });
+    });
+  },
 };
